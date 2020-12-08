221c1118d428e4320bbe80c821a2532df114e91a
T1MON <derbindima5@gmail.com>
2020-12-08 20:47:37 +0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+
+
+class SpaceBattle:
+    def __init__(self):
+        pass  # Window initialisation
+
+    def start(self):
+        pass  # Starting the game
+
+
+if __name__ == "__main__":
+    game = SpaceBattle()
+    game.start()
