0d777c2f5f5fe785d6db49a53ed20c2f4daadcd2
T1MON <derbindima5@gmail.com>
2020-12-11 20:50:01 +0400

    Finished image.py Added rocket textures

--- managers/image.py
+++ managers/image.py
@@ -7,11 +7,27 @@ class Image:
     """
     Class containing objects' textures, already preparing for using
     """
-    SHIP = pg.image.load(f"./resources/textures/ship/{Conf.Images.SHIP}.{Conf.Images.FORMAT}").convert_alpha()
-    ROCKET = pg.image.load(f"./resources/textures/rocket/{Conf.Images.ROCKET}.{Conf.Images.FORMAT}").convert_alpha()
-    METEOR = list(
-        map(
-            lambda x: pg.image.load(f"./resources/textures/meteor/{x}.{Conf.Images.FORMAT}").convert_alpha(),
-            range(*Conf.Images.METEOR)
-        )
-    )
+    __ROOT = "./resources/textures"
+    __SHIP = None
+    __ROCKET = None
+    __METEOR = None
+
+    @staticmethod
+    def get_ship() -> pg.image:
+        if Image.__SHIP is None:
+            Image.__SHIP = pg.image.load(f"{Image.__ROOT}/ship/{Conf.Images.SHIP}.{Conf.Images.FORMAT}").convert_alpha()
+        return Image.__SHIP
+
+    @staticmethod
+    def get_rocket() -> pg.image:
+        if Image.__ROCKET is None:
+            Image.__ROCKET = pg.image.load(f"{Image.__ROOT}/rocket/{Conf.Images.ROCKET}.{Conf.Images.FORMAT}").convert_alpha()
+        return Image.__ROCKET
+
+    @staticmethod
+    def get_meteor() -> list[pg.image]:
+        if Image.__METEOR is None:
+            Image.__METEOR = []
+            for x in range(*Conf.Images.METEOR):
+                Image.__METEOR.append(pg.image.load(f"{Image.__ROOT}/meteor/{x}.{Conf.Images.FORMAT}").convert_alpha())
+        return Image.__METEOR
