b47b8b2b4e4f338eb8ffc09e3e8c427b22e0839e
Amikuto <Amikuto@users.noreply.github.com>
2020-12-12 18:50:12 +0300

    Some menu comments
    Added comments on the menu, loaded the background.

--- components/menu.py
+++ components/menu.py
@@ -11,13 +11,16 @@ class Menu:
 
         # Initialisation
         # TODO: Damir
-        pygame_menu.themes.THEME_DEFAULT.widget_font = pygame_menu.font.FONT_OPEN_SANS
-        self.create_about()
-        self.create_menu()
+        pygame_menu.themes.THEME_DEFAULT.widget_font = pygame_menu.font.FONT_OPEN_SANS  # Setting the default font
+        self.create_about()  # Create the about menu
+        self.create_menu()  # Create the main menu
 
     def create_about(self):
         """
         Create menus: About
+        This function contains an about list that displays text on the screen.
+        A separate about theme is created by copying the standard one,
+        the theme is customized - the font of the title is changed.
         """
         ABOUT = ['Game version: {0}'.format(Conf.Menu.GAME_VERSION),
                  f'Authors: {Conf.Menu.AUTHORS[0]}, {Conf.Menu.AUTHORS[1]}, {Conf.Menu.AUTHORS[2]}',
@@ -25,25 +28,31 @@ class Menu:
                  'Ideas and suggestions: SpaceBattle@ya.com']
 
         about_theme = pygame_menu.themes.THEME_DEFAULT.copy()
-        about_theme.widget_margin = (0, 0)
-        about_theme.widget_offset = (0, 0.05)
         about_theme.title_font_size = 56
 
         self.about_menu = pygame_menu.Menu(
             height=Conf.Menu.HEIGHT,
-            onclose=pygame_menu.events.DISABLE_CLOSE,
-            theme=about_theme,
-            title='About',
-            width=Conf.Menu.WIDTH
+            width=Conf.Menu.WIDTH,
+            onclose=pygame_menu.events.DISABLE_CLOSE,  # Action on closing
+            theme=about_theme,  # Setting theme
+            title='About'
         )
+
         for m in ABOUT:
             self.about_menu.add_label(m, align=pygame_menu.locals.ALIGN_LEFT, font_size=30)
         self.about_menu.add_label('')
-        self.about_menu.add_button('Return to menu', pygame_menu.events.BACK, selection_color=(0, 0, 0), offset=(0, 300))
+        self.about_menu.add_button(
+            'Return to menu',
+            pygame_menu.events.BACK,
+            selection_color=(0, 0, 0),
+        )
 
     def create_menu(self):
         """
-        Create menus: Main
+        Create menus: Main. Responsible for setting up the main menu.
+        A picture is selected for the background. Customizable theme, colors, font size, etc.
+        A name entry line is added, a play button that redirects to the start function of the game,
+        similarly with the about and exit buttons.
         """
 
         # TODO : добавить в менеджер картинок
@@ -53,7 +62,7 @@ class Menu:
 
         my_theme = pygame_menu.themes.Theme(
             selection_color=(0, 250, 0),
-            title_bar_style=pygame_menu.widgets.MENUBAR_STYLE_NONE,
+            title_bar_style=pygame_menu.widgets.MENUBAR_STYLE_NONE,  # Separating header and body
             title_offset=(Conf.Window.WIDTH/5, 0),
             title_font_color=(255, 255, 255),
             title_font_size=64,
@@ -61,7 +70,7 @@ class Menu:
             widget_font_color=(255, 255, 255),
             widget_font_size=40,
             widget_margin=(0, 40),
-            menubar_close_button=False
+            menubar_close_button=False  # Removing the close button
         )
 
         self.menu = pygame_menu.Menu(
