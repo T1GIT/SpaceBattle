0183163803b42a0d2da8bace8b53ae0b17af93b1
T1MON <derbindima5@gmail.com>
2020-12-13 14:18:28 +0400

    Working on ship rotating Added fullscreen

--- elements/ship.py
+++ elements/ship.py
@@ -1,5 +1,5 @@
 import pygame as pg
-from math import cos, sin, tan, acos, asin, atan, sqrt, pi
+from math import cos, sin, tan, acos, asin, atan2, sqrt, pi, degrees, atan
 
 from config import Configuration as Conf
 from managers.image import Image as Img
@@ -17,10 +17,12 @@ class Ship(pg.sprite.Sprite):
         w0, h0 = raw_image.get_size()
         scale = Conf.Ship.SIZE / h0
         w1, h1 = map(lambda x: round(x * scale), [w0, h0])
-        self.image = pg.transform.scale(raw_image, (w1, h1))
+        self.texture = pg.transform.scale(raw_image, (w1, h1))
+        self.image = self.texture.copy()
         self._max_speed = sqrt(Conf.Ship.POWER / Conf.Ship.RESIST)
         self.x_speed = 0
         self.y_speed = 0
+        self.angle = 0
 
     def locate(self, x, y):
         """
@@ -40,29 +42,42 @@ class Ship(pg.sprite.Sprite):
         speed = sqrt(pow(self.x_speed, 2) + pow(self.y_speed, 2))
         a_x, a_y = 0, 0
         if speed != 0:
-            rad = self.get_angle(self.x_speed, self.y_speed)
+            rad = atan2(self.y_speed, self.x_speed)
             r = Conf.Ship.RESIST * pow(speed, 2)
             a_x -= r * cos(rad)
             a_y -= r * sin(rad)
         # Adding accel
-        gas = min(1.0, sqrt(pow(x, 2) + pow(y, 2)))
         if x != 0 or y != 0:
-            rad = self.get_angle(x, y)
+            rad = atan2(y, x)
             f = Conf.Ship.POWER
-            a_x += f * cos(rad)
-            a_y += f * sin(rad)
+            a_x += f * cos(rad) * pow(x, 2)
+            a_y += f * sin(rad) * pow(y, 2)
         self.x_speed += a_x / Conf.Ship.WEIGHT
         self.y_speed += a_y / Conf.Ship.WEIGHT
 
-    @staticmethod
-    def get_angle(x, y):
-        if x == 0:
-            if y > 0: rad = pi / 2
-            else: rad = pi * 3 / 2
-        elif y == 0:
-            rad = 0
-        else:
-            rad = atan(y / x)
-        if x < 0: rad += pi
-        return rad
+    def rotate(self, x, y):
+        move = atan2(y, x)
+        self.angle = degrees(move)
+
+        self.image = pg.transform.rotate(self.texture, self.angle - 90)
+        self.rect = self.image.get_rect(center=self.rect.center)
+        # if abs(d_deg) > 100 / Conf.Ship.ACCURACY:
+        #     self.angle += d_deg / Conf.Ship.SMOOTH
+        #     self.angle = (self.angle + 180) % 360 - 180
+        #     self.image = pg.transform.rotate(self.texture, self.angle - 90)
+        #     self.rect = self.image.get_rect(center=self.rect.center)
+
+        # move = degrees(self.__get_angle(x, y))
+        # if abs(move - self.angle) > 180 and abs(self.angle) > 90 and abs(move) > 90:
+        #     d_deg = (360 - abs(self.angle - move)) * -1 if self.angle < move else 1
+        #     print(1)
+        # else:
+        #     d_deg = move - self.angle
+
+        # print(move, d_deg, self.angle)
+        # if abs(d_deg) > 100 / Conf.Ship.ACCURACY:
+        #     self.angle += d_deg / Conf.Ship.SMOOTH
+        #     self.angle = (self.angle + 180) % 360 - 180
+        #     self.image = pg.transform.rotate(self.texture, self.angle - 90)
+        #     self.rect = self.image.get_rect(center=self.rect.center)
 
