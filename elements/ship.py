07fec874545bca00f97ce7a90376aa69487b2ca1
T1MON <derbindima5@gmail.com>
2020-12-13 09:53:34 +0400

    Ship moving is working

--- elements/ship.py
+++ elements/ship.py
@@ -1,5 +1,5 @@
 import pygame as pg
-from math import cos, sin
+from math import cos, sin, tan, acos, asin, atan, sqrt, pi
 
 from config import Configuration as Conf
 from managers.image import Image as Img
@@ -18,6 +18,9 @@ class Ship(pg.sprite.Sprite):
         scale = Conf.Ship.SIZE / h0
         w1, h1 = map(lambda x: round(x * scale), [w0, h0])
         self.image = pg.transform.scale(raw_image, (w1, h1))
+        self._max_speed = sqrt(Conf.Ship.POWER / Conf.Ship.RESIST)
+        self.x_speed = 0
+        self.y_speed = 0
 
     def locate(self, x, y):
         """
@@ -29,4 +32,37 @@ class Ship(pg.sprite.Sprite):
             center=(x, y))
 
     def update(self):
-        pass
+        self.rect.x += round(self.x_speed)
+        self.rect.y -= round(self.y_speed)
+
+    def accelerate(self, x, y):
+        # Adding resistance
+        speed = sqrt(pow(self.x_speed, 2) + pow(self.y_speed, 2))
+        a_x, a_y = 0, 0
+        if speed != 0:
+            rad = self.get_angle(self.x_speed, self.y_speed)
+            r = Conf.Ship.RESIST * pow(speed, 2)
+            a_x -= r * cos(rad)
+            a_y -= r * sin(rad)
+        # Adding accel
+        gas = min(1.0, sqrt(pow(x, 2) + pow(y, 2)))
+        if x != 0 or y != 0:
+            rad = self.get_angle(x, y)
+            f = Conf.Ship.POWER
+            a_x += f * cos(rad)
+            a_y += f * sin(rad)
+        self.x_speed += a_x / Conf.Ship.WEIGHT
+        self.y_speed += a_y / Conf.Ship.WEIGHT
+
+    @staticmethod
+    def get_angle(x, y):
+        if x == 0:
+            if y > 0: rad = pi / 2
+            else: rad = pi * 3 / 2
+        elif y == 0:
+            rad = 0
+        else:
+            rad = atan(y / x)
+        if x < 0: rad += pi
+        return rad
+
