709beeaf0cd703a29de729200ae2b1540e4e757e
T1MON <derbindima5@gmail.com>
2020-12-15 20:01:50 +0400

    Corrected and clean upd

--- managers/sound.py
+++ managers/sound.py
@@ -7,30 +7,55 @@ class Sound:
     """
     Class playing sounds of the game
     """
-    # TODO: Damir
-    def __init__(self):
-        pg.mixer.init()
+    _ROOT = "./resources/sounds"
+    _VOLUME = Conf.Sound.Volume
 
     @staticmethod
-    def menu_music():
-        pg.mixer.music.load('./resources/sounds/menu/SW-opening.mp3')
-        pg.mixer.music.set_volume(Conf.Sounds.BACKGROUND_MENU)
+    def get_volume(volume: float) -> float:
+        return volume * Conf.Sound.Volume.GENERAL / 100
+
+    """
+    SFX
+    """
+    @staticmethod
+    def click():
+        return f"{Sound._ROOT}/sfx/click/{Conf.Sound.CLICK}.{Conf.Sound.FORMAT}"
+
+    @staticmethod
+    def shoot():
+        fire_sound = pg.mixer.Sound(
+            f'{Sound._ROOT}/sfx/shoot/{Conf.Sound.SHOOT}.{Conf.Sound.FORMAT}')
+        fire_sound.set_volume(Sound.get_volume(Sound._VOLUME.SHOOT))
+        fire_sound.play()
+
+    """
+    BACKGROUND
+    """
+    @staticmethod
+    def bg_menu():
+        pg.mixer.music.load(
+            f'{Sound._ROOT}/background/menu/{Conf.Sound.BG_MENU}.{Conf.Sound.FORMAT}')
+        pg.mixer.music.set_volume(Sound.get_volume(Sound._VOLUME.BG_MENU))
         pg.mixer.music.play(-1)
 
     @staticmethod
-    def fire_sound():
-        fire_sound = pg.mixer.Sound('./resources/sounds/fire/TIEfire.mp3')
-        fire_sound.set_volume(Conf.Sounds.FIRE)
+    def bg_game():
+        pg.mixer.music.load(
+            f'{Sound._ROOT}/background/game/{Conf.Sound.BG_GAME}.{Conf.Sound.FORMAT}')
+        pg.mixer.music.set_volume(Sound.get_volume(Sound._VOLUME.BG_GAME))
+        pg.mixer.music.play(-1)
+
+    """
+    EXPLODE
+    """
+    @staticmethod
+    def ex_player():
+        fire_sound = pg.mixer.Sound(f'{Sound._ROOT}/explode/player.{Conf.Sound.FORMAT}')
+        fire_sound.set_volume(Sound.get_volume(Sound._VOLUME.EX_PLAYER))
         fire_sound.play()
 
     @staticmethod
-    def game_music():
-        pg.mixer.music.load('./resources/sounds/bg/kosmos.mp3')
-        pg.mixer.music.set_volume(Conf.Sounds.BACKGROUND_GAME)
-        pg.mixer.music.play(-1)
-
-    @staticmethod
-    def player_explode():
-        fire_sound = pg.mixer.Sound('./resources/sounds/explode/Player_explode.mp3')
-        fire_sound.set_volume(Conf.Sounds.EXPLODE_ASTEROIDS)
+    def ex_meteor():
+        fire_sound = pg.mixer.Sound(f'{Sound._ROOT}/explode/meteor.{Conf.Sound.FORMAT}')
+        fire_sound.set_volume(Sound.get_volume(Sound._VOLUME.EX_METEORS))
         fire_sound.play()
